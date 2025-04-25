955602a91b3ece54a5d9619e69d64c0cc737e217
kaykysoarex <kaykysoaresbahia@gmail.com>
2025-04-24 23:19:48 -0300

    Add files via upload
    criando portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+// Aqui você pode adicionar interatividade, como animações ou efeitos
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso!");
+});
+
